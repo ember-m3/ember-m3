75dce5425c4a7c7cf3b3bc6ba56ead7410fb2d33
David J. Hamilton <dhamilton@linkedin.com>
2017-04-18 12:49:11 -0700

    linting

--- tests/dummy/app/serializers/application.js
+++ tests/dummy/app/serializers/application.js
@@ -1,7 +1,7 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPISerializer.extend({
-  normalizeResponse(store, primaryModelClass, payload, id, requestType) {
+  normalizeResponse(store, primaryModelClass, payload /*, id, requestType */) {
     return payload;
   },
 
