f59642b2c65faf5f3dae38cc4d289987b5c663e2
Robert Jackson <rjackson@linkedin.com>
2017-08-23 11:59:27 -0400

    Drop support for Ember < 2.12.

--- config/ember-try.js
+++ config/ember-try.js
@@ -34,38 +34,6 @@ module.exports = {
       }
     },
     {
-      name: 'ember-lts-2.4',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#lts-2-4'
-        },
-        resolutions: {
-          'ember': 'lts-2-4'
-        }
-      },
-      npm: {
-        devDependencies: {
-          'ember-source': null
-        }
-      }
-    },
-    {
-      name: 'ember-lts-2.8',
-      bower: {
-        dependencies: {
-          'ember': 'components/ember#lts-2-8'
-        },
-        resolutions: {
-          'ember': 'lts-2-8'
-        }
-      },
-      npm: {
-        devDependencies: {
-          'ember-source': null
-        }
-      }
-    },
-    {
       name: 'ember-lts-2.12',
       npm: {
         devDependencies: {
