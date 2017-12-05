3bd335067f64fe7eaa98c09c88af483c603ca4dc
Chris Thoburn <cthoburn@linkedin.com>
2017-12-05 14:04:30 -0800

    prevent assert.throws errors from affecting us again

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,8 +1,11 @@
+import Ember from 'ember';
 import resolver from './helpers/resolver';
 import {
   setResolver
 } from 'ember-qunit';
 import { start } from 'ember-cli-qunit';
+
+Ember.Test.adapter.exception = (reason) => { throw reason; };
 
 setResolver(resolver);
 start();
