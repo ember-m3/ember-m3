a802b9908ed92f958bb3ac45f77b4548dbd2b618
Danail Nachev <dnachev@linkedin.com>
2018-02-15 10:54:50 -0800

    model data tests #1

--- tests/unit/model-data-test.js
+++ tests/unit/model-data-test.js
@@ -1,19 +1,30 @@
 import { module, test } from 'qunit';
 import M3ModelData from 'ember-m3/model-data';
+import SchemaManager from 'ember-m3/schema-manager';
+
+const modelDataKey = (modelName, id) => `${modelName}:${id}`;
 
 module('unit/model-data', function(hooks) {
   hooks.beforeEach(function() {
-    this.store = null;
-    this.storeWrapper = null;
+    let storeWrapper = (this.storeWrapper = {
+      modelDatas: {},
+      modelDataFor(modelName, id) {
+        let key = modelDataKey(modelName, id);
+        return (
+          this.modelDatas[key] ||
+          (this.modelDatas[key] = new M3ModelData(modelName, id, null, storeWrapper))
+        );
+      },
+    });
+
+    SchemaManager.registerSchema({
+      computeBaseModelName(modelName) {
+        return modelName === 'com.bookstore.projected-book' ? 'com.bookstore.book' : null;
+      },
+    });
 
     this.mockModelData = function() {
-      return new M3ModelData({
-        store: this.store,
-        modelName: 'com.bookstore.book',
-        clientId: null,
-        id: '1',
-        storeWrapper: this.storeWrapper,
-      });
+      return this.storeWrapper.modelDataFor('com.bookstore.book', '1');
     };
   });
 
@@ -42,6 +53,44 @@ module('unit/model-data', function(hooks) {
     assert.notOk(
       typeof schemaInterface.setAttr === 'function',
       'schemaInterface cannot write attr'
+    );
+  });
+
+  test('`.getOrCreateNestedModelData` reuses model datas', function(assert) {
+    let modelData = this.mockModelData();
+    let nestedModelData = modelData.getOrCreateNestedModelData(
+      'preface',
+      'com.bookstore.chapter',
+      '1',
+      null
+    );
+
+    assert.notEqual(nestedModelData, null, 'Expected nested model data to be returned');
+
+    let anotherNestedModelData = modelData.getOrCreateNestedModelData(
+      'preface',
+      'com.bookstore.chapter',
+      '1',
+      null
+    );
+
+    assert.strictEqual(
+      nestedModelData,
+      anotherNestedModelData,
+      'The nested model data must be the same'
+    );
+  });
+
+  test('projection model data initializes and register in base model data', function(assert) {
+    let projectedModelData = this.storeWrapper.modelDataFor('com.bookstore.projected-book', '1');
+
+    let baseModelData = this.storeWrapper.modelDatas[modelDataKey('com.bookstore.book', '1')];
+
+    assert.notEqual(baseModelData, null, 'Expected base model data to be initialized');
+    assert.deepEqual(
+      baseModelData._projections,
+      [baseModelData, projectedModelData],
+      'Expected projected model data to be in the projections list'
     );
   });
 });
