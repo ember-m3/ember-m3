1c95caa71973c0e816b231d068587942d3fcb104
David J. Hamilton <dhamilton@linkedin.com>
2018-02-22 09:20:30 -0800

    Add eslint plugin ember

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     ecmaVersion: 2017,
     sourceType: 'module',
   },
-  extends: ['eslint:recommended', 'prettier'],
+  extends: ['eslint:recommended', 'prettier', 'plugin:ember/recommended'],
   env: {
     browser: true,
   },
