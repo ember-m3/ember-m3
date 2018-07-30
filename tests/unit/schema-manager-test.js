6346e5cea2e6d54f75dacbaf8af068e362e67172
Danail Nachev <dnachev@linkedin.com>
2018-07-30 09:45:29 -0700

    Cleanup the old schema manager, which is now a service

--- tests/unit/schema-manager-test.js
+++ tests/unit/schema-manager-test.js
@@ -1,7 +1,10 @@
 import { module, test } from 'qunit';
-import SchemaManager from 'ember-m3/schema-manager';
+import SchemaManager from 'ember-m3/services/m3-schema-manager';
 
-module('unit/schema-manager', function() {
+module('unit/schema-manager', function(hooks) {
+  hooks.beforeEach(function() {
+    this.schemaManager = SchemaManager.create();
+  });
   module('.registerSchema', function() {
     test('it allows only a single global schema', function(assert) {
       let schema1 = {
@@ -22,30 +25,30 @@ module('unit/schema-manager', function() {
         models: {},
       };
 
-      SchemaManager.registerSchema(schema1);
-      assert.ok(SchemaManager.includesModel('m1'), 'schema1 reigstered (includes m1)');
-      assert.notOk(SchemaManager.includesModel('m2'), 'schema1 reigstered (!includes m2)');
+      this.schemaManager.registerSchema(schema1);
+      assert.ok(this.schemaManager.includesModel('m1'), 'schema1 reigstered (includes m1)');
+      assert.notOk(this.schemaManager.includesModel('m2'), 'schema1 reigstered (!includes m2)');
 
-      SchemaManager.registerSchema(schema2);
-      assert.notOk(SchemaManager.includesModel('m1'), 'schema2 reigstered (!includes m1)');
-      assert.ok(SchemaManager.includesModel('m2'), 'schema2 reigstered (includes m2)');
+      this.schemaManager.registerSchema(schema2);
+      assert.notOk(this.schemaManager.includesModel('m1'), 'schema2 reigstered (!includes m1)');
+      assert.ok(this.schemaManager.includesModel('m2'), 'schema2 reigstered (includes m2)');
     });
   });
 
   module('schemas', function() {
     test('can specify what models the schema handles', function(assert) {
-      SchemaManager.registerSchema({
+      this.schemaManager.registerSchema({
         includesModel(modelName) {
           return /com\.example\.bookstore\.*/i.test(modelName);
         },
       });
 
-      assert.equal(SchemaManager.includesModel('com.example.bookstore.Book'), true);
-      assert.equal(SchemaManager.includesModel('com.example.petstore.Pet'), false);
+      assert.equal(this.schemaManager.includesModel('com.example.bookstore.Book'), true);
+      assert.equal(this.schemaManager.includesModel('com.example.petstore.Pet'), false);
     });
 
     test('can specify what fields refer to other models in the store', function(assert) {
-      SchemaManager.registerSchema({
+      this.schemaManager.registerSchema({
         computeAttributeReference(key, value) {
           if (/^ref-/i.test(key)) {
             return {
@@ -57,26 +60,26 @@ module('unit/schema-manager', function() {
         },
       });
 
-      assert.deepEqual(SchemaManager.computeAttributeReference('ref-foo', 200), {
+      assert.deepEqual(this.schemaManager.computeAttributeReference('ref-foo', 200), {
         type: 'foo',
         id: 200,
       });
-      assert.deepEqual(SchemaManager.computeAttributeReference('foo', 70), null);
+      assert.deepEqual(this.schemaManager.computeAttributeReference('foo', 70), null);
     });
 
     test('can specify a nested model matcher', function(assert) {
-      SchemaManager.registerSchema({
+      this.schemaManager.registerSchema({
         computeNestedModel(key) {
           return /com\.example\./i.test(key);
         },
       });
 
-      assert.equal(SchemaManager.computeNestedModel('com.example.bookstore.Author'), true);
-      assert.equal(SchemaManager.computeNestedModel('name'), false);
+      assert.equal(this.schemaManager.computeNestedModel('com.example.bookstore.Author'), true);
+      assert.equal(this.schemaManager.computeNestedModel('name'), false);
     });
 
     test('can specify per-modelName transforms', function(assert) {
-      SchemaManager.registerSchema({
+      this.schemaManager.registerSchema({
         models: {
           'com.example.bookstore.Book': {
             transforms: {
@@ -89,21 +92,21 @@ module('unit/schema-manager', function() {
       });
 
       assert.equal(
-        SchemaManager.transformValue('com.example.bookstore.Book', 'name', 'jeff'),
+        this.schemaManager.transformValue('com.example.bookstore.Book', 'name', 'jeff'),
         'jeff OMG!'
       );
       assert.equal(
-        SchemaManager.transformValue('com.example.bookstore.Book', 'alternateName', 'jeff'),
+        this.schemaManager.transformValue('com.example.bookstore.Book', 'alternateName', 'jeff'),
         'jeff'
       );
       assert.equal(
-        SchemaManager.transformValue('com.example.bookstore.Author', 'name', 'jeff'),
+        this.schemaManager.transformValue('com.example.bookstore.Author', 'name', 'jeff'),
         'jeff'
       );
     });
 
     test('can specify per-modelName whitelisted attributes', function(assert) {
-      SchemaManager.registerSchema({
+      this.schemaManager.registerSchema({
         models: {
           'com.example.bookstore.Book': {
             attributes: ['name'],
@@ -115,34 +118,49 @@ module('unit/schema-manager', function() {
         },
       });
 
-      assert.equal(SchemaManager.isAttributeIncluded('com.example.bookstore.Book', 'name'), true);
-      assert.equal(SchemaManager.isAttributeIncluded('com.example.bookstore.Book', 'age'), false);
-      assert.equal(SchemaManager.isAttributeIncluded('com.example.bookstore.Author', 'name'), true);
-      assert.equal(SchemaManager.isAttributeIncluded('com.example.bookstore.Author', 'age'), true);
       assert.equal(
-        SchemaManager.isAttributeIncluded('com.example.bookstore.ReaderComment', 'name'),
+        this.schemaManager.isAttributeIncluded('com.example.bookstore.Book', 'name'),
         true
       );
       assert.equal(
-        SchemaManager.isAttributeIncluded('com.example.bookstore.ReaderComment', 'age'),
+        this.schemaManager.isAttributeIncluded('com.example.bookstore.Book', 'age'),
+        false
+      );
+      assert.equal(
+        this.schemaManager.isAttributeIncluded('com.example.bookstore.Author', 'name'),
         true
       );
       assert.equal(
-        SchemaManager.isAttributeIncluded('com.example.bookstore.SearchResult', 'name'),
+        this.schemaManager.isAttributeIncluded('com.example.bookstore.Author', 'age'),
         true
       );
       assert.equal(
-        SchemaManager.isAttributeIncluded('com.example.bookstore.SearchResult', 'age'),
+        this.schemaManager.isAttributeIncluded('com.example.bookstore.ReaderComment', 'name'),
+        true
+      );
+      assert.equal(
+        this.schemaManager.isAttributeIncluded('com.example.bookstore.ReaderComment', 'age'),
+        true
+      );
+      assert.equal(
+        this.schemaManager.isAttributeIncluded('com.example.bookstore.SearchResult', 'name'),
+        true
+      );
+      assert.equal(
+        this.schemaManager.isAttributeIncluded('com.example.bookstore.SearchResult', 'age'),
         true
       );
     });
   });
 
   test('.isAttributeIncluded does not error when no schema is registered', function(assert) {
-    assert.equal(SchemaManager.isAttributeIncluded('com.example.movies.Movie', 'name'), true);
+    assert.equal(this.schemaManager.isAttributeIncluded('com.example.movies.Movie', 'name'), true);
   });
 
   test('.transformValue does not error when no schema is registered', function(assert) {
-    assert.equal(SchemaManager.transformValue('com.example.moves.Movie', 'name', 'jeff'), 'jeff');
+    assert.equal(
+      this.schemaManager.transformValue('com.example.moves.Movie', 'name', 'jeff'),
+      'jeff'
+    );
   });
 });
