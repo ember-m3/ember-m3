6f735eefeb6ee53a330e272a7bbe1c02874d4e20
Chris Thoburn <cthoburn@linkedin.com>
2018-10-23 11:12:01 -0700

    bugfix

--- addon/-private.js
+++ addon/-private.js
@@ -3,7 +3,7 @@ import { IS_RECORD_DATA } from 'ember-compatibility-helpers';
 export function recordDataFor(recordOrInternalModel) {
   let internalModel = recordOrInternalModel._internalModel || recordOrInternalModel;
 
-  if (IS_RECORD_DATA) {
+  if (!IS_RECORD_DATA) {
     return internalModel._modelData;
   }
 
