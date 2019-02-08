91e0d30aaf3be182c88b245bfd9e71a877b7468d
David J. Hamilton <dhamilton@linkedin.com>
2019-02-07 18:36:02 -0800

    chore: add commitlint

---
+++ .commitlintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  rules: {
+    'body-leading-blank': [2, 'always'],
+    'footer-leading-blank': [2, 'always'],
+    'header-max-length': [2, 'always', 72],
+    'scope-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': [2, 'always', ['chore', 'docs', 'feat', 'fix', 'perf', 'refactor', 'test']],
+  },
+};
