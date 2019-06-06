080d2593d9e0efd8011bf8ba57be2f1a769381f5
David J. Hamilton <dhamilton@linkedin.com>
2019-06-05 17:19:30 -0700

    chore: drop support for node 6, ember 2.18
    Updating to match last 2 LTS and current node LTS.  Also yarn upgrade &
    toolchain → volta (`toolchain` is deprecated as part of the notion →
    volta switch).

--- config/ember-try.js
+++ config/ember-try.js
@@ -17,18 +17,18 @@ module.exports = function() {
           npm: {},
         },
         {
-          name: 'ember-lts-2.18',
-          npm: {
-            devDependencies: {
-              'ember-source': '~2.18.2',
-            },
-          },
-        },
-        {
           name: 'ember-lts-3.4',
           npm: {
             devDependencies: {
               'ember-source': '~3.4.5',
+            },
+          },
+        },
+        {
+          name: 'ember-lts-3.8',
+          npm: {
+            devDependencies: {
+              'ember-source': '~3.8.0',
             },
           },
         },
