d8c34231b24e971323b1bcff5c55b52ba0aa06b1
Chris Thoburn <cthoburn@linkedin.com>
2019-10-15 16:56:00 -0500

    fix release versions

--- tests/integration/interop-debug-adapter-test.js
+++ tests/integration/interop-debug-adapter-test.js
@@ -7,6 +7,9 @@ import { capitalize, underscore } from '@ember/string';
 import { A } from '@ember/array';
 import InteropDebugAdapter from 'ember-m3/adapters/interop-debug-adapter';
 import DefaultSchema from 'ember-m3/services/m3-schema';
+import { has } from 'require';
+
+const HasDebugAdapterPackage = has('@ember-data/debug');
 
 const BOOK_MODEL_TYPE = 'com.example.bookstore.Book';
 const NEW_MODEL_TYPE = 'com.example.newModel';
@@ -146,150 +149,302 @@ module('integration/interop-debug-adapter', function(hooks) {
     );
   });
 
-  test('watchModelTypes correctly watches both m3 and DS.Model records', async function(assert) {
-    assert.expect(7);
+  if (HasDebugAdapterPackage) {
+    test('watchModelTypes correctly watches both m3 and DS.Model records', async function(assert) {
+      assert.expect(7);
 
-    let typesAddedCallCount = 0;
-    let typesUpdatedCallCount = 0;
-    const genreClass = this.owner.factoryFor('model:genre').class;
-    const m3TypesAdded = [
-      {
-        name: 'com.example.bookstore.book',
-        count: 1,
-        columns: generateM3Columns(['id', '$type', 'name', 'author', 'pubDate', 'readerComments']),
-        object: 'com.example.bookstore.book',
-      },
-    ];
-
-    const dsTypesAdded1 = [
-      {
-        name: 'publisher',
-        count: 1,
-        columns: generateDSColumns(['id', 'name', 'foundedDate']),
-        object: this.owner.factoryFor('model:publisher').class,
-      },
-    ];
-
-    const dsTypesAdded2 = [
-      {
-        columns: generateDSColumns(['id', 'name', 'description']),
-        count: 0,
-        name: 'genre',
-        object: genreClass,
-      },
-    ];
-
-    const newM3TypesAdded = [
-      {
-        name: 'com.example.new-model',
-        count: 1,
-        columns: generateM3Columns(['id', '$type', 'name']),
-        object: 'com.example.new-model',
-      },
-    ];
-
-    const newDSTypesUpdated = [
-      {
-        columns: generateDSColumns(['id', 'name', 'description']),
-        count: 1,
-        name: 'genre',
-        object: genreClass,
-      },
-    ];
-
-    // TODO: Fix test to ensure `typesUpdated` gets called with resolved m3 records
-    // accessing attributes on the record and using the run loop does not work
-    const m3TypesUpdated = [
-      {
-        name: 'com.example.bookstore.book',
-        count: 0,
-        columns: generateM3Columns(['id']),
-        object: 'com.example.bookstore.book',
-      },
-    ];
-
-    const newM3TypesUpdated = [
-      {
-        name: 'com.example.new-model',
-        count: 0,
-        columns: generateM3Columns(['id']),
-        object: 'com.example.new-model',
-      },
-    ];
-
-    const typesAdded = typesToSend => {
-      typesAddedCallCount++;
-      switch (typesAddedCallCount) {
-        case 1:
-          return assert.deepEqual(
-            typesToSend,
-            m3TypesAdded,
-            'Correct type object passed into typesAdded initially for m3 record types'
-          );
-        case 2:
-          return assert.deepEqual(
-            typesToSend,
-            dsTypesAdded1,
-            'Correct type object passed into typesAdded for DS.Model record types'
-          );
-        case 3:
-          return assert.deepEqual(
-            typesToSend,
-            dsTypesAdded2,
-            'Correct type object passed into typesAdded for DS.Model record types'
-          );
-        case 4:
-          return assert.deepEqual(
-            typesToSend,
-            newM3TypesAdded,
-            'Correct type object passed into typesAdded for new m3 record types'
-          );
-        default:
-          return null;
-      }
-    };
-
-    const typesUpdated = updatedTypesToSend => {
-      typesUpdatedCallCount++;
-      switch (typesUpdatedCallCount) {
-        case 1:
-          return assert.deepEqual(
-            updatedTypesToSend,
-            newDSTypesUpdated,
-            'Correct type object passed into typesUpdated when new DS.Model records are added'
-          );
-        case 2:
-          return assert.deepEqual(
-            updatedTypesToSend,
-            m3TypesUpdated,
-            'Correct type object passed into typesUpdated initially for m3 record types'
-          );
-        case 3:
-          return assert.deepEqual(
-            updatedTypesToSend,
-            newM3TypesUpdated,
-            'Correct type object passed into typesUpdated for new m3 record types'
-          );
-        default:
-          return null;
-      }
-    };
-
-    this.interopDebugAdapter.watchModelTypes(typesAdded, typesUpdated);
-
-    this.store.pushPayload(NEW_MODEL_TYPE, NEW_MODEL_DATA);
-
-    this.store.push({
-      data: {
-        id: 'urn:genre:1',
-        type: 'genre',
-        attributes: {
-          name: 'Horror',
-          description: 'Fiction that will keep you up at night.',
+      let typesAddedCallCount = 0;
+      let typesUpdatedCallCount = 0;
+      const genreClass = this.owner.factoryFor('model:genre').class;
+      const m3TypesAdded = [
+        {
+          name: 'com.example.bookstore.book',
+          count: 1,
+          columns: generateM3Columns([
+            'id',
+            '$type',
+            'name',
+            'author',
+            'pubDate',
+            'readerComments',
+          ]),
+          object: 'com.example.bookstore.book',
         },
-      },
+      ];
+
+      const dsTypesAdded1 = [
+        {
+          name: 'publisher',
+          count: 1,
+          columns: generateDSColumns(['id', 'name', 'foundedDate']),
+          object: this.owner.factoryFor('model:publisher').class,
+        },
+      ];
+
+      const dsTypesAdded2 = [
+        {
+          columns: generateDSColumns(['id', 'name', 'description']),
+          count: 0,
+          name: 'genre',
+          object: genreClass,
+        },
+      ];
+
+      const newM3TypesAdded = [
+        {
+          name: 'com.example.new-model',
+          count: 1,
+          columns: generateM3Columns(['id', '$type', 'name']),
+          object: 'com.example.new-model',
+        },
+      ];
+
+      const newDSTypesUpdated = [
+        {
+          columns: generateDSColumns(['id', 'name', 'description']),
+          count: 1,
+          name: 'genre',
+          object: genreClass,
+        },
+      ];
+
+      // TODO: Fix test to ensure `typesUpdated` gets called with resolved m3 records
+      // accessing attributes on the record and using the run loop does not work
+      const m3TypesUpdated = [
+        {
+          name: 'com.example.bookstore.book',
+          count: 0,
+          columns: generateM3Columns(['id']),
+          object: 'com.example.bookstore.book',
+        },
+      ];
+
+      const newM3TypesUpdated = [
+        {
+          name: 'com.example.new-model',
+          count: 0,
+          columns: generateM3Columns(['id']),
+          object: 'com.example.new-model',
+        },
+      ];
+
+      const typesAdded = typesToSend => {
+        typesAddedCallCount++;
+        switch (typesAddedCallCount) {
+          case 1:
+            return assert.deepEqual(
+              typesToSend,
+              m3TypesAdded,
+              'Correct type object passed into typesAdded initially for m3 record types'
+            );
+          case 2:
+            return assert.deepEqual(
+              typesToSend,
+              dsTypesAdded1,
+              'Correct type object passed into typesAdded for DS.Model record types'
+            );
+          case 3:
+            return assert.deepEqual(
+              typesToSend,
+              dsTypesAdded2,
+              'Correct type object passed into typesAdded for DS.Model record types'
+            );
+          case 4:
+            return assert.deepEqual(
+              typesToSend,
+              newM3TypesAdded,
+              'Correct type object passed into typesAdded for new m3 record types'
+            );
+          default:
+            return null;
+        }
+      };
+
+      const typesUpdated = updatedTypesToSend => {
+        typesUpdatedCallCount++;
+        switch (typesUpdatedCallCount) {
+          case 1:
+            return assert.deepEqual(
+              updatedTypesToSend,
+              newDSTypesUpdated,
+              'Correct type object passed into typesUpdated when new DS.Model records are added'
+            );
+          case 2:
+            return assert.deepEqual(
+              updatedTypesToSend,
+              m3TypesUpdated,
+              'Correct type object passed into typesUpdated initially for m3 record types'
+            );
+          case 3:
+            return assert.deepEqual(
+              updatedTypesToSend,
+              newM3TypesUpdated,
+              'Correct type object passed into typesUpdated for new m3 record types'
+            );
+          default:
+            return null;
+        }
+      };
+
+      this.interopDebugAdapter.watchModelTypes(typesAdded, typesUpdated);
+
+      this.store.pushPayload(NEW_MODEL_TYPE, NEW_MODEL_DATA);
+
+      this.store.push({
+        data: {
+          id: 'urn:genre:1',
+          type: 'genre',
+          attributes: {
+            name: 'Horror',
+            description: 'Fiction that will keep you up at night.',
+          },
+        },
+      });
     });
-  });
+  } else {
+    test('watchModelTypes correctly watches both m3 and DS.Model records', async function(assert) {
+      assert.expect(6);
+
+      let typesAddedCallCount = 0;
+      let typesUpdatedCallCount = 0;
+      const genreClass = this.owner.factoryFor('model:genre').class;
+      const m3TypesAdded = [
+        {
+          name: 'com.example.bookstore.book',
+          count: 1,
+          columns: generateM3Columns([
+            'id',
+            '$type',
+            'name',
+            'author',
+            'pubDate',
+            'readerComments',
+          ]),
+          object: 'com.example.bookstore.book',
+        },
+      ];
+
+      const dsTypesAdded = [
+        {
+          name: 'publisher',
+          count: 1,
+          columns: generateDSColumns(['id', 'name', 'foundedDate']),
+          object: this.owner.factoryFor('model:publisher').class,
+        },
+        {
+          columns: generateDSColumns(['id', 'name', 'description']),
+          count: 0,
+          name: 'genre',
+          object: genreClass,
+        },
+      ];
+
+      const newM3TypesAdded = [
+        {
+          name: 'com.example.new-model',
+          count: 1,
+          columns: generateM3Columns(['id', '$type', 'name']),
+          object: 'com.example.new-model',
+        },
+      ];
+
+      const newDSTypesUpdated = [
+        {
+          columns: generateDSColumns(['id', 'name', 'description']),
+          count: 1,
+          name: 'genre',
+          object: genreClass,
+        },
+      ];
+
+      // TODO: Fix test to ensure `typesUpdated` gets called with resolved m3 records
+      // accessing attributes on the record and using the run loop does not work
+      const m3TypesUpdated = [
+        {
+          name: 'com.example.bookstore.book',
+          count: 0,
+          columns: generateM3Columns(['id']),
+          object: 'com.example.bookstore.book',
+        },
+      ];
+
+      const newM3TypesUpdated = [
+        {
+          name: 'com.example.new-model',
+          count: 0,
+          columns: generateM3Columns(['id']),
+          object: 'com.example.new-model',
+        },
+      ];
+
+      const typesAdded = typesToSend => {
+        typesAddedCallCount++;
+        switch (typesAddedCallCount) {
+          case 1:
+            return assert.deepEqual(
+              typesToSend,
+              m3TypesAdded,
+              'Correct type object passed into typesAdded initially for m3 record types'
+            );
+          case 2:
+            return assert.deepEqual(
+              typesToSend,
+              dsTypesAdded,
+              'Correct type object passed into typesAdded for DS.Model record types'
+            );
+          case 3:
+            return assert.deepEqual(
+              typesToSend,
+              newM3TypesAdded,
+              'Correct type object passed into typesAdded for new m3 record types'
+            );
+          default:
+            return null;
+        }
+      };
+
+      const typesUpdated = updatedTypesToSend => {
+        typesUpdatedCallCount++;
+        switch (typesUpdatedCallCount) {
+          case 1:
+            return assert.deepEqual(
+              updatedTypesToSend,
+              newDSTypesUpdated,
+              'Correct type object passed into typesUpdated when new DS.Model records are added'
+            );
+          case 2:
+            return assert.deepEqual(
+              updatedTypesToSend,
+              m3TypesUpdated,
+              'Correct type object passed into typesUpdated initially for m3 record types'
+            );
+          case 3:
+            return assert.deepEqual(
+              updatedTypesToSend,
+              newM3TypesUpdated,
+              'Correct type object passed into typesUpdated for new m3 record types'
+            );
+          default:
+            return null;
+        }
+      };
+
+      this.interopDebugAdapter.watchModelTypes(typesAdded, typesUpdated);
+
+      this.store.pushPayload(NEW_MODEL_TYPE, NEW_MODEL_DATA);
+
+      this.store.push({
+        data: {
+          id: 'urn:genre:1',
+          type: 'genre',
+          attributes: {
+            name: 'Horror',
+            description: 'Fiction that will keep you up at night.',
+          },
+        },
+      });
+    });
+  }
 
   test('typesUpdated is called when new m3 and DS.Model records are added of an existing type', async function(assert) {
     assert.expect(2);
