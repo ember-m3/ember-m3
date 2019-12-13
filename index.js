d5f503fc543d9e4fd7328442b288fbdd748408a9
David J. Hamilton <dhamilton@linkedin.com>
2019-12-12 19:49:04 -0800

    Fix ember data feature import
    The feature import was based on Ember Data 3.14, which is not the
    current canary.
    
    This commit still depends on an unstable import path
    `@ember-data/private-build-infra/src/features`.
    
    Co-authored-by: Robert Jackson <me@rwjblue.com>

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 /* eslint-env node */
 'use strict';
 
+const resolve = require('resolve');
+
 function debugMacros(features) {
   let plugins = [
     [
@@ -20,18 +22,33 @@ function debugMacros(features) {
   return plugins;
 }
 
+function enabledFeatures(isDevelopingAddon) {
+  let features = {
+    CUSTOM_MODEL_CLASS: false,
+  };
+  try {
+    let emberDataPath = require.resolve('ember-data');
+    let buildInfra = resolve.sync('@ember-data/private-build-infra/src/features', {
+      cwd: emberDataPath,
+    });
+    let emberDataFeatures = require(buildInfra)(process.env.EMBER_ENV === 'production');
+    if ('CUSTOM_MODEL_CLASS' in emberDataFeatures) {
+      features.CUSTOM_MODEL_CLASS = emberDataFeatures.CUSTOM_MODEL_CLASS;
+    }
+  } catch (e) {
+    features = { CUSTOM_MODEL_CLASS: isDevelopingAddon ? null : false };
+  }
+
+  return features;
+}
+
 module.exports = {
   name: 'ember-m3',
 
   included() {
     this._super.included.apply(this, arguments);
 
-    let features;
-    try {
-      features = this.project.require('@ember-data/private-build-infra/src/features')();
-    } catch (e) {
-      features = { CUSTOM_MODEL_CLASS: this.isDevelopingAddon() ? null : false };
-    }
+    let features = enabledFeatures(this.isDevelopingAddon());
 
     this.options = this.options || {};
     this.options.babel = this.options.babel || {};
