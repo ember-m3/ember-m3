c4433a739524083e865a162fb6a7d26c0c823ba7
Igor Terzic <iterzic@linkedin.com>
2021-06-18 12:54:34 -0500

    initial perf testing app

---
+++ m3-perf-testing-app/tests/test-helper.js
@@ -0,0 +1,12 @@
+import Application from 'm3-perf-testing-app/app';
+import config from 'm3-perf-testing-app/config/environment';
+import * as QUnit from 'qunit';
+import { setApplication } from '@ember/test-helpers';
+import { setup } from 'qunit-dom';
+import { start } from 'ember-qunit';
+
+setApplication(Application.create(config.APP));
+
+setup(QUnit.assert);
+
+start();
