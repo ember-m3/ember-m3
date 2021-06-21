f0296e5027744efed5d563dc2c6192aed2955f7f
Igor Terzic <iterzic@linkedin.com>
2021-06-21 18:10:36 -0500

    render

--- m3-perf-testing-app/app/routes/pushing.js
+++ m3-perf-testing-app/app/routes/pushing.js
@@ -3,7 +3,9 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model() {
     performance.mark('start-loading');
+    window.setTimeout(() => {
     performance.mark('end-loading');
+    }, 100);
     return {};
   },
 });
