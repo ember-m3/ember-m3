4ee851426e304630f351464b73584cc3745cbf4a
Igor Terzic <iterzic@linkedin.com>
2021-06-21 18:31:27 -0500

    how aobut now

--- m3-perf-testing-app/app/routes/pushing.js
+++ m3-perf-testing-app/app/routes/pushing.js
@@ -3,9 +3,10 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model() {
     performance.mark('start-loading');
-    window.setTimeout(() => {
-      performance.mark('end-loading');
-    }, 100);
     return { message: 'hello'};
   },
+
+  didTransition() {
+      performance.mark('end-loading');
+  }
 });
