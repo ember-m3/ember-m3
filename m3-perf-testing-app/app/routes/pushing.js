f51160ebd0b1b5f7abc776c2d9e88fa016ff279b
Igor Terzic <iterzic@linkedin.com>
2021-06-21 16:58:23 -0500

    perf flag name

--- m3-perf-testing-app/app/routes/pushing.js
+++ m3-perf-testing-app/app/routes/pushing.js
@@ -3,7 +3,7 @@ import Route from '@ember/routing/route';
 export default Route.extend({
   model() {
     performance.mark('start-loading');
-    performance.mark('stop-loading');
+    performance.mark('end-loading');
     return {};
   },
 });
