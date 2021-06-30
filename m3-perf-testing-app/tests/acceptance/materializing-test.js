d92207d7471392675147c41b8c9fba1ad93035ad
Igor Terzic <iterzic@linkedin.com>
2021-06-30 15:34:37 -0700

    Make perfomance testing app more robust to slow tests

--- m3-perf-testing-app/tests/acceptance/materializing-test.js
+++ m3-perf-testing-app/tests/acceptance/materializing-test.js
@@ -21,7 +21,7 @@ module('Acceptance | materializing', function (hooks) {
     searches.forEach((search, i) => {
       // Sample the array for correctness, otherwise the test generates too
       // many assertions and take too long
-      if (i % 50 !== 0) {
+      if (i % 100 !== 0) {
         return;
       }
       let results = search.get('results');
