dc1107a4f54d517343cffc86bffc75371e8c2e06
Igor Terzic <iterzic@linkedin.com>
2021-08-13 02:22:23 -0500

    dont target ie11

--- m3-perf-testing-app/config/targets.js
+++ m3-perf-testing-app/config/targets.js
@@ -9,10 +9,6 @@ const browsers = [
 const isCI = Boolean(process.env.CI);
 const isProduction = process.env.EMBER_ENV === 'production';
 
-if (isCI || isProduction) {
-  browsers.push('ie 11');
-}
-
 module.exports = {
   browsers,
 };
