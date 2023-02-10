2ba0f896e41715c50750284ac14af79b764eb928
David J. Hamilton <dhamilton@linkedin.com>
2023-02-10 11:20:09 -0800

    Fix cache merging
    Nested models would not previously be merged under the following
    conditions:
      1. the nested models were projections of base models
      2. the userland `computeBaseModelName` hook returned an undasherized
         model name for nested models
    
    This was due to a missing call to dasherize, to keep names normalized
    for Ember Data.

--- tests/dummy/app/serializers/application.js
+++ tests/dummy/app/serializers/application.js
@@ -10,6 +10,18 @@ export default class ApplicationSerializer {
     return store.push(payload);
   }
 
+  serialize(snapshot) {
+    let result = {};
+    snapshot.eachAttribute((k) => {
+      let value = snapshot.record.get(k);
+      if (value && typeof value.serialize === 'function') {
+        value = value.serialize();
+      }
+      result[k] = value;
+    });
+    return result;
+  }
+
   static create(createArgs) {
     return new this(createArgs);
   }
